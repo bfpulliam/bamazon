3047389590481ee06a3a2b599bbe4952da7d3dbf
bfpulliam <bfpulliam@gmail.com>
2019-04-15 22:09:16 -0500

    bamazon working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -26,74 +26,83 @@ function initBamazon() {
     // Create the db query string
     queryStr = 'SELECT item_id, product_name, price FROM products';
     // Make the db query
+     
     connection.query(queryStr, function (err, data) {
         if (err) throw err;
         for (var i = 0; i < data.length; i++) {
             var strItems = '';
-            strItems += 'Item ID:' + data[i].item_id + ' ' + data[i].product_name + ' - '+ 'Price: $' + data[i].price + '\n';
+            strItems += 'Item ID:' + data[i].item_id + ' ' + data[i].product_name + ' - ' + 'Price: $' + data[i].price + '\n';
             console.log(strItems);
         }
         start();
     });
-    
+
 }
-// function validateID(name) {
-//     if (name  === item_id) {
-//         console.log(name);
-//     } else {
-//         console.log('Please enter a valid item id.');
-//     }UPDATE `bamazon_db`.`products` SET `stock_quantity` = '25' WHERE (`item_id` = '1');
+
+//UPDATE `bamazon_db`.`products` SET `stock_quantity` = '25' WHERE (`item_id` = '1');
 // }
 
+function updateStock(quanity, stock_id) {
+    
+    var newQuanity = stock_quanity -quanity;
+    var sqlUpdate = "UPDATE products SET stock_quantity = " + newQuanity + " WHERE item_id = " + answer;
+
+    // execute the UPDATE statement
+    connection.query(sqlUpdate, function (err, res) {
+        if (err) throw err;
+        console.log('Rows affected:', results.affectedRows);
+    });
+    connection.end();
+}
+
+function validateID(x) {
+    if (isNaN(x) || x < 1 || x > 10) {
+        console.log("Input not valid");
+        return false;
+    } else {
+        console.log("Thank you");
+        return true;
+    }
+}
+
+function validateQuanity(x) {
+    if (x > stock_quanity + 1){
+        console.log("Insufficient inventory!!");
+        return false;
+    }else{
+        console.log("Thank you for your purchase!");
+        return true;
+    }
+}
 
 // function which prompts the user for what action they should take
 function start() {
 
     inquirer
         .prompt({
-            type: "input",
+            type: "number",
             message: "What is the item_id number of the item that you wish to purchase?",
             name: "stock_id",
-            // validate: validateID
-        }).then(function(answer) {
+            filter: Number,
+            validate: validateID
+        }).then(function (answer) {
             console.log(answer);
-        inquirer
-            .prompt({
-                type: "input",
-                message: "How many do you want?",
-                name:"quanity"
-            })
-            .then(function (quanity) {
-                console.log(quanity);
-##########################################################################
-                if (quanity === stock_quanity){
-                    console.log("You have purchased " + quanity +"!");
-                    var newquanity = (stock_quanity - quanity);
-                    console.log(newquanity);
-                    // inventory is high enough, so update db, let the user know, and start over
-                    connection.query(
-                        
-                        "UPDATE products SET ? WHERE ?",
-                        [
-                            {
-                                stock_quanity: newquanity
-                            },
-                            {
-                                item_id: answer
-                            }
-                        ],
-                        function (error) {
-                            if (error) throw err;
-                            console.log("Purchase successfully!");
-                            initBamazon();
-                        }
-                    );
-                
-                }
-                else{
-                    console.log("Insufficient quantity!");
-                    initBamazon();
-                }
+            var queryQuanity = 'SELECT stock_quanity FROM products WHERE item_id='+ stock_id;
+            // Make the db query
+            connection.query(queryQuanity, function (err, data) {
+                if (err) throw err;
+                var stock_quanity = data;
             });
-    });
+            inquirer
+                .prompt({
+                    type: "number",
+                    message: "How many do you want?",
+                    name: "quanity",
+                    filter: Number,
+                    validate: validateQuanity
+                });
+        });
 }
+        
+
+
