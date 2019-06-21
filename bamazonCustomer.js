1c0a75c143d136385173237c9824e6607590c628
Bettina Pulliam <bfpulliam@gmail.com>
2019-06-21 02:47:40 -0500

    Bamazon Customers are now able to make purchase.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,115 +1,81 @@
-require('dotenv').config();
+require("dotenv").config();
 sqlpword = process.env.sqlpword;
-var mysql = require('mysql');
-var inquirer = require('inquirer');
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
-    host: 'localhost',
+    host: "localhost",
     // connection port
     port: 3306,
     //username
-    user: 'root',
+    user: "root",
     //password import from env
     password: sqlpword,
-    database: 'bamazon_db'
-});
-// connect to the mysql server and sql database
-connection.connect(function (err) {
-    if (err) throw err;
-    //  run the function after the connection is made to prompt the user
-    initBamazon();
+    database: "bamazon_db"
 });
 
-//show items at Bamazon
 function initBamazon() {
-    // Create the db query string
-    queryStr = 'SELECT item_id, product_name, price FROM products';
-    // Make the db query
-
-    connection.query(queryStr, function (err, data) {
+    connection.connect(function (err) {
         if (err) throw err;
-        for (var i = 0; i < data.length; i++) {
-            var strItems = '';
-            strItems += 'Item ID: ' + data[i].item_id + ' ' + data[i].product_name + ' - ' + 'Price: $' + data[i].price + '\n';
-            console.log(strItems);
-        }
-        begin();
+        console.log("connected as id " + connection.threadId);
+        displayItems();
     });
-
 }
-var questions = [{
-    type: 'number',
-    message: 'What is the item_id number of the item that you wish to purchase?',
-    name: 'stock_id',
-}, {
-    type: 'number',
-    message: 'How many do you want?',
-    name: 'procure',
-}];
-function begin () {
-    inquirer.prompt(questions).then(function(data){
-       var stock_id = parseInt(data.stock_id);
-       var procure = data.procure;
-       qryStr = 'SELECT stock_quantity, product_name, price FROM products WHERE item_id = 1';
-
-       connection.query(qryStr, [stock_id], function(answers){
-           console.log(answers);
-
-       });
+function displayItems() {
+    connection.query("SELECT item_id, product_name, price, stock_quantity FROM products", function (err, res) {
+        if (err) throw err;
+        console.log("WELCOME TO BAMAZON");
+        console.table(res);
+        purchaseItem();
     });
 }
 
-//     //     connection.query(queryquantity, function () {
-//     //         console.log('Inventory' + stock_quantity);
-//     //         var inventory = stock_quantity;
-//     //         var product = stock_id;
-//     //         if (err) throw err;
-//     //         if (parseInt(product.procure) <= inventory) {
-//     //             var newQuanity = (inventory - procure);
-//     //             connection.query('UPDATE products SET ? WHERE ?', [{
-//     //                 stock_quantity: newQuanity
-//     //             }, {
-//     //                 item_id: product
-//     //             }], function (err, res) { });
-//     //         } else {
-//     //             console.log('Opps!');
-//     //         }
-//     //     });
-//     // });
-//     //     })
 
-//     // }));
+function purchaseItem() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        inquirer.prompt([
+            {
+                message: "What is the item_id number of the item that you wish to purchase?",
+                name: "stock_id",
+                type: "input"
+            },
+            {
+                message: "How much would you like to buy?",
+                name: "purchase_amount",
+                type: "input"
+            }
+        ]).then(function (user) {
+            //connect to database to find stock_quantity in database. If user quantity input is greater than stock, decline purchase.
 
+            connection.query("SELECT * FROM products WHERE item_id=?", user.stock_id, function (err, res) {
+                for (var i = 0; i < res.length; i++) {
 
+                    if (user.purchase_amount > res[i].stock_quantity) {
+                        console.log("There are not enough items in inventory.  Please select again");
+                        purchaseItem();
+                    } else {
+                        //list item information for user for confirm prompt
+                        console.log("You've selected: " + res[i].product_name + ".  The cost of the item is: " + res[i].price);
+                        console.log("Total: " + res[i].price * user.purchase_amount);
+                        var newStock = (res[i].stock_quantity - user.purchase_amount);
+                        var purchaseId = (user.stock_id);
+                        connection.query("UPDATE products SET ? WHERE ?", [{
+                            stock_quantity: newStock
+                        }, {
+                            item_id: purchaseId
+                        }], function (err, res) {
+                            console.log("Thank you for your purchase. We have " + newStock + " left.");
+                            displayItems();
+                        });
 
-// //UPDATE `bamazon_db`.`products` SET `stock_quantity` = '25' WHERE (`item_id` = '1');
-// // }
+                    }
+                }
+            });
+        });
+    });
+}
 
-// // // function which prompts the user for what action they should take
-
-
-// // function start() {
-
-// //     // inquirer.prompt(questions, function (answers) {
-// //     //     console.log(answers[0]);
-//     //     // console.log(answers[0].stock_id);
-//     //     // console.log(answers[0].procure);
-//     //     var queryquantity = 'SELECT stock_quantity FROM products WHERE item_id=' + answers[0].stock_id;
-//     //     connection.query(queryquantity, function () {
-//     //         console.log('Inventory' + stock_quantity);
-//     //         var inventory = stock_quantity;
-//     //         var product = stock_id;
-//     //         if (err) throw err;
-//     //         if (parseInt(product.procure) <= inventory) {
-//     //             var newQuanity = (inventory - procure);
-//     //             connection.query('UPDATE products SET ? WHERE ?', [{
-//     //                 stock_quantity: newQuanity
-//     //             }, {
-//     //                 item_id: product
-//     //             }], function (err, res) { });
-//     //         } else {
-//     //             console.log('Opps!');
-//     //         }
-//     //     });
-//     // })
+initBamazon();
